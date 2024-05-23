98055c26928467158580d8cd915bf543ee55c72f
alpharii <alphari404@gmail.com>
2024-05-23 16:04:28 +0700

    fix bug

--- src/Components/Elements/Buttons.jsx
+++ src/Components/Elements/Buttons.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 
 const Buttons = (props) => {
-    if(props.background === ''){
-        props.background = slate
-    }
   return (
     <button class={`bg-${props.background}-500 hover:bg-${props.background}-700 shadow-2xl text-${props.text} font-bold py-2 px-4 rounded-lg w-96 h-12`}>
         {props.children}
