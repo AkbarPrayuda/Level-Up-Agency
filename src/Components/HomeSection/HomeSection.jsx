3515947bee2f2cd413beca9c037ee535623b2598
Akbar Prayuda <akbarprayudaputra21@gmail.com>
2024-05-24 18:37:06 +0700

    Fix Button

--- src/Components/HomeSection/HomeSection.jsx
+++ src/Components/HomeSection/HomeSection.jsx
@@ -10,8 +10,8 @@ const HomeSection = () => {
             <div className='flex flex-col items-center md:w-1/2 gap-9'>
                 <h1 className='py-4 text-3xl font-semibold text-center md:text-start md:text-2xl lg:text-5xl md:py-0'>Membawa Bisnis Lokal ke Tingkat Berikutnya, Melalui Era Digital</h1>
                 <div className='flex flex-col items-center gap-4 md:gap-7'>
-                    <Buttons text='text-white' background='lime-main' backgroundHover='lime-main'>Hubungi Kami</Buttons>
-                    <Buttons text="text-blue-400" background='white' backgroundHover='stone-200'>Konsultasi Sekarang</Buttons>
+                    <Buttons text="text-white" bg="bg-lime-main" bgHover="bg-lime-main/50">Hubungi Kami</Buttons>
+                    <Buttons text="text-blue-400" bg="bg-white" bgHover="bg-stone-200/50">Konsultasi Sekarang</Buttons>
                 </div>
             </div>
         </div>
