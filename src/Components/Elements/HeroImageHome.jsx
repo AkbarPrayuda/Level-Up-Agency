6337527206f24479dbc6339583df80036cd29ab8
Akbar Prayuda <akbarprayudaputra21@gmail.com>
2024-05-24 19:27:36 +0700

    Try Fix Img

--- src/Components/Elements/HeroImageHome.jsx
+++ src/Components/Elements/HeroImageHome.jsx
@@ -1,6 +1,6 @@
 const HeroImageHome = () => {
   return (
-    <img src={'../../public/img/HeroImageHome.png'} alt="" className='w-auto h-64 lg:h-96 md:h-72 rounded-3xl'/>
+    <img src={'./src/assets/img/HeroImageHome.png'} alt="" className='w-auto h-64 lg:h-96 md:h-72 rounded-3xl'/>
   )
 }
 
