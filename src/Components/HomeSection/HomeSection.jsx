4b353d36b97b0aabe71d9a2da3262b3332ae2634
Akbar Prayuda <akbarprayudaputra21@gmail.com>
2024-05-25 07:31:54 +0700

    Try Main Text

--- src/Components/HomeSection/HomeSection.jsx
+++ src/Components/HomeSection/HomeSection.jsx
@@ -9,7 +9,7 @@ const HomeSection = () => {
         <div className='flex flex-col items-center justify-between h-full md:flex-row md:gap-9 lg:gap-9' id='home'>
             <HeroImageHome/>
             <div className='flex flex-col items-center md:w-1/2 gap-9'>
-                <h1 className='py-4 text-3xl font-semibold text-center md:text-start md:text-2xl lg:text-5xl md:py-0'>Membawa Bisnis Lokal ke Tingkat Berikutnya, Melalui Era Digital</h1>
+                <h1 className='py-4 text-2xl font-semibold text-center md:text-start lg:text-5xl md:py-0'>Membawa Bisnis Lokal ke Tingkat Berikutnya, Melalui Era Digital</h1>
                 <div className='flex flex-col items-center gap-4 md:gap-7'>
                     <Buttons text="text-white" bg="bg-lime-main">Hubungi Kami</Buttons>
                     <WhiteButton text="text-blue-400">Konsultasi Sekarang</WhiteButton>
