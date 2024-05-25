57bcdebeceee0a2a7f5364141179786d5cb0a1be
Akbar Prayuda <akbarprayudaputra21@gmail.com>
2024-05-25 12:00:57 +0700

    Fixed About 2

--- src/Components/AboutSection/AboutSection2.jsx
+++ src/Components/AboutSection/AboutSection2.jsx
@@ -8,7 +8,7 @@ function AboutSection2() {
         <div className='flex flex-col gap-6 text-center md:text-start'>
             <h1 className='text-4xl font-semibold md:text-4xl lg:text-6xl text-lime-main font-poppins'>Buat Website anda Sekarang!</h1>
             <p className='text-2xl font-semibold'>
-              ciptakan website anda untuk meningkatkan jangkauan dari bisnis anda dengan cepat dengan digitalisasi dan teknologi digital
+              ciptakan website anda untuk meningkatkan jangkauan dari bisnis anda dengan cepat melalui digitalisasi dan teknologi digital
             </p>
         </div>
     </div>
