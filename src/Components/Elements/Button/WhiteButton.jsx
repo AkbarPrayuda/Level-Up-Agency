b6a49ab2c09b7b8ad1b74b868953a14494a12576
Akbar Prayuda <akbarprayudaputra21@gmail.com>
2024-05-25 07:30:11 +0700

    Fixed Button

--- src/Components/Elements/Button/WhiteButton.jsx
+++ src/Components/Elements/Button/WhiteButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WhiteButton = (props) => {
   return (
-    <button class={`bg-white hover:bg-stone-100 ${props.text} font-semibold rounded-lg md:text-xl lg:text-2xl text-2xl w-96 md:w-72 md:h-12 h-16 lg:w-96 lg:h-16 shadow-xl border-black`}>
+    <button class={`bg-white hover:bg-stone-100 ${props.text} font-semibold rounded-lg md:text-xl lg:text-2xl text-2xl w-72 md:w-72 md:h-12 h-16 lg:w-96 lg:h-16 shadow-xl border-black`}>
         {props.children}
     </button>
   )
